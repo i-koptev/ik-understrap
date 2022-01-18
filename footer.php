43ec184db3bebcf4a31c7d6e30b4fb01e0827e3f
Igor Koptev <igor.koptef@gmail.com>
2022-01-18 22:02:04 +0200

    Files added: header.php and footer.php

---
+++ footer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+//$container = get_theme_mod( 'understrap_container_type' );
+$container = 'container';
+?>
+
+<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_attr( $container ); ?>">
+
+		<div class="row">
+
+			<div class="col-md-12">
+
+				<footer class="site-footer" id="colophon">
+
+					<div class="site-info">
+						FOOTER!
+
+<!--						--><?php //understrap_site_info(); ?>
+
+					</div><!-- .site-info -->
+
+				</footer><!-- #colophon -->
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
+
