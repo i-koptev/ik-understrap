c45369755bdf8973c6459a8daa10300eff1be1b1
Igor Koptev <igor.koptef@gmail.com>
2022-01-21 14:37:09 +0200

    Front-page file added

---
+++ front-page.php
@@ -0,0 +1,110 @@
+<?php
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+$container = 'container';
+//$container = get_theme_mod( 'ikunderstrap_container_type' );
+?>
+
+	<div class="wrapper" id="index-wrapper">
+
+		<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+			<div class="row">
+
+				<!-- Do the left sidebar check and opens the primary div -->
+				<!--			--><?php //get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+				<main class="site-main" id="main">
+					<h1>Title 1</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis magnam neque officiis quasi
+						rem repellendus sapiente similique voluptatibus. Aliquam est id iusto labore quasi reiciendis
+						ullam! Accusantium blanditiis dolorum expedita illum iste iure labore molestiae necessitatibus
+						nemo odit optio quas sequi soluta, ullam unde? Corporis doloremque error non officia optio. Ab
+						ad adipisci aspernatur assumenda distinctio dolor, dolorum explicabo incidunt magnam mollitia
+						necessitatibus obcaecati omnis sapiente suscipit tempora voluptas voluptatum? Adipisci, aperiam
+						asperiores beatae cum cumque dolore eius error eum hic illo laboriosam minima nemo obcaecati
+						optio placeat quam recusandae reiciendis temporibus vel voluptates! Accusamus ad adipisci
+						commodi consequatur delectus dolor earum eveniet hic illo, ipsum iste libero molestiae nemo
+						nostrum provident quo quod, repellat sunt unde ut veniam vero voluptate voluptatem? Accusantium
+						architecto asperiores culpa, cumque debitis dicta dolore eveniet expedita inventore iste maxime
+						modi natus neque nisi non omnis quaerat quibusdam recusandae repudiandae, unde velit vitae
+						voluptatum. At, dolorem laborum! Adipisci amet, delectus dicta eum fuga impedit iste nulla odio
+						officiis placeat possimus quaerat soluta tempore, velit vero. Accusantium ad alias delectus
+						distinctio dolore eius eligendi, error, harum in inventore ipsum laudantium magni omnis
+						perspiciatis quaerat quasi quis recusandae tempore unde veniam! Mollitia nobis nulla odit quasi
+						voluptate.</p>
+
+
+					<h1>Title 2</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis magnam neque officiis quasi
+						rem repellendus sapiente similique voluptatibus. Aliquam est id iusto labore quasi reiciendis
+						ullam! Accusantium blanditiis dolorum expedita illum iste iure labore molestiae necessitatibus
+						nemo odit optio quas sequi soluta, ullam unde? Corporis doloremque error non officia optio. Ab
+						ad adipisci aspernatur assumenda distinctio dolor, dolorum explicabo incidunt magnam mollitia
+						necessitatibus obcaecati omnis sapiente suscipit tempora voluptas voluptatum? Adipisci, aperiam
+						asperiores beatae cum cumque dolore eius error eum hic illo laboriosam minima nemo obcaecati
+						optio placeat quam recusandae reiciendis temporibus vel voluptates! Accusamus ad adipisci
+						commodi consequatur delectus dolor earum eveniet hic illo, ipsum iste libero molestiae nemo
+						nostrum provident quo quod, repellat sunt unde ut veniam vero voluptate voluptatem? Accusantium
+						architecto asperiores culpa, cumque debitis dicta dolore eveniet expedita inventore iste maxime
+						modi natus neque nisi non omnis quaerat quibusdam recusandae repudiandae, unde velit vitae
+						voluptatum. At, dolorem laborum! Adipisci amet, delectus dicta eum fuga impedit iste nulla odio
+						officiis placeat possimus quaerat soluta tempore, velit vero. Accusantium ad alias delectus
+						distinctio dolore eius eligendi, error, harum in inventore ipsum laudantium magni omnis
+						perspiciatis quaerat quasi quis recusandae tempore unde veniam! Mollitia nobis nulla odit quasi
+						voluptate.</p>
+					<h1>Title 3</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis magnam neque officiis quasi
+						rem repellendus sapiente similique voluptatibus. Aliquam est id iusto labore quasi reiciendis
+						ullam! Accusantium blanditiis dolorum expedita illum iste iure labore molestiae necessitatibus
+						nemo odit optio quas sequi soluta, ullam unde? Corporis doloremque error non officia optio. Ab
+						ad adipisci aspernatur assumenda distinctio dolor, dolorum explicabo incidunt magnam mollitia
+						necessitatibus obcaecati omnis sapiente suscipit tempora voluptas voluptatum? Adipisci, aperiam
+						asperiores beatae cum cumque dolore eius error eum hic illo laboriosam minima nemo obcaecati
+						optio placeat quam recusandae reiciendis temporibus vel voluptates! Accusamus ad adipisci
+						commodi consequatur delectus dolor earum eveniet hic illo, ipsum iste libero molestiae nemo
+						nostrum provident quo quod, repellat sunt unde ut veniam vero voluptate voluptatem? Accusantium
+						architecto asperiores culpa, cumque debitis dicta dolore eveniet expedita inventore iste maxime
+						modi natus neque nisi non omnis quaerat quibusdam recusandae repudiandae, unde velit vitae
+						voluptatum. At, dolorem laborum! Adipisci amet, delectus dicta eum fuga impedit iste nulla odio
+						officiis placeat possimus quaerat soluta tempore, velit vero. Accusantium ad alias delectus
+						distinctio dolore eius eligendi, error, harum in inventore ipsum laudantium magni omnis
+						perspiciatis quaerat quasi quis recusandae tempore unde veniam! Mollitia nobis nulla odit quasi
+						voluptate.</p>
+					<h1>Title 4</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis magnam neque officiis quasi
+						rem repellendus sapiente similique voluptatibus. Aliquam est id iusto labore quasi reiciendis
+						ullam! Accusantium blanditiis dolorum expedita illum iste iure labore molestiae necessitatibus
+						nemo odit optio quas sequi soluta, ullam unde? Corporis doloremque error non officia optio. Ab
+						ad adipisci aspernatur assumenda distinctio dolor, dolorum explicabo incidunt magnam mollitia
+						necessitatibus obcaecati omnis sapiente suscipit tempora voluptas voluptatum? Adipisci, aperiam
+						asperiores beatae cum cumque dolore eius error eum hic illo laboriosam minima nemo obcaecati
+						optio placeat quam recusandae reiciendis temporibus vel voluptates! Accusamus ad adipisci
+						commodi consequatur delectus dolor earum eveniet hic illo, ipsum iste libero molestiae nemo
+						nostrum provident quo quod, repellat sunt unde ut veniam vero voluptate voluptatem? Accusantium
+						architecto asperiores culpa, cumque debitis dicta dolore eveniet expedita inventore iste maxime
+						modi natus neque nisi non omnis quaerat quibusdam recusandae repudiandae, unde velit vitae
+						voluptatum. At, dolorem laborum! Adipisci amet, delectus dicta eum fuga impedit iste nulla odio
+						officiis placeat possimus quaerat soluta tempore, velit vero. Accusantium ad alias delectus
+						distinctio dolore eius eligendi, error, harum in inventore ipsum laudantium magni omnis
+						perspiciatis quaerat quasi quis recusandae tempore unde veniam! Mollitia nobis nulla odit quasi
+						voluptate.</p>
+				</main><!-- #main -->
+
+				<!-- The pagination component -->
+				<!--			--><?php //understrap_pagination(); ?>
+
+				<!-- Do the right sidebar check -->
+				<!--			--><?php //get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+			</div><!-- .row -->
+
+		</div><!-- #content -->
+
+	</div><!-- #index-wrapper -->
+
+<?php
+get_footer();
